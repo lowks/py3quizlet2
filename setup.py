3db2f3d4a12ee4bf907184cab429a5dd6609c013
Low Kian Seong <kianseong@gmail.com>
2014-12-06 21:46:47 +0800

    Update setup.py
    Adding more metadata to `setup.py` to be picked up by `pypi`

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from distutils.core import setup
 
 setup(name='py3quizlet2',
       version='0.1',
+      description='Python wrapper for Quizlet API',
+      url='https://github.com/hammer/py3quizlet2',
       py_modules=['py3quizlet2'],
       )
